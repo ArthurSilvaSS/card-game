3598a448e0460f9352d454ed4e9ba2660a9fbbe9
ArthurSilvaSS <arthur.silva443@gmail.com>
2023-05-09 15:31:17 -0300

    trabalho na rotação 3D do cartão

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,14 +1,17 @@
 import CardGame from "../CardGame";
+import "./style.css";
 
-function CardFrontBack(){
-    return /*html*/ `
-         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("js","logo do javascript")}
-        </article>
-    
-    `
-
+function CardFrontBack() {
+  return /*html*/ `
+    <article class="card-front-back">
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame("javascript", "Logo do JavaScript")}
+      </div>
+    </article>
+  `;
 }
 
 export default CardFrontBack;
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,11 @@
-import './style.css'
-function CardGame(icon = 'alura-pixel', alt = 'logo da alura') {
-    return /*html*/`
-    <article class="card-game">
-        <img src="img/${icon}.png"alt="${alt}">
-    </article>
+import "./style.css";
 
-    `
+function CardGame(icon = "alura-pixel", alt = "Logo da Alura") {
+  return /*html*/ `
+    <article class="card-game">
+      <img src="img/${icon}.png" alt="${alt}">
+    </article>
+  `;
 }
 
 export default CardGame;
