10d96ae92e7fc2989a7acc7371c2e58972c864fb
ArthurSilvaSS <arthur.silva443@gmail.com>
2023-05-08 23:10:02 -0300

    style do board-game

--- src/objects/BordGame/index.js
+++ src/objects/BordGame/index.js
@@ -1,11 +1,18 @@
+import './style.css'
 import CardGame from './../../components/CardGame';
 
 function BordGame(amountCards){
 
     const $htmlCardGame = CardGame();//variavel recebendo o conteudo
-    const $htmlBordGame = $htmlCardGame.repeat(amountCards)//propriedade repeat para repeit uma string
+    const $htmlContent = $htmlCardGame.repeat(amountCards)//propriedade repeat para repeit uma string
 
-    return $htmlBordGame;
+    return `
+        <section class = "board-game">
+            ${$htmlContent}
+        </section>
+    
+    `;
+     
 }
 
 export default BordGame;
