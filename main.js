8d0985480b91199f8aeb40960ad9fea614638a60
ArthurSilvaSS <arthur.silva443@gmail.com>
2023-05-09 08:25:14 -0300

    Criando o componente do player

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ import './src/styles/settings/colors.css';
 import'./src/styles/generic/reset.css';
 import'./src/styles/elements/base.css';
 import BoardGame from './src/objects/BordGame'
+import PlayerName from './src/components/PlayerName';
 
 const $root = document.querySelector("#root");
-const $htmlBordGame = BoardGame(6)
 
-
-$root.insertAdjacentHTML('beforeend', $htmlBordGame) 
+$root.insertAdjacentHTML(
+    "beforeend",
+    `
+        ${PlayerName("Player1")}
+        ${PlayerName("Player2")}
+        ${BoardGame(6)}
+    `
+    ) 
